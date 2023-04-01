285b9249e0b29d1e66281232bc1faad6e1d684c3
Rawbethy <robert@Valhalla.localdomain>
2023-03-31 23:43:39 -0500

    033123

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -122,37 +122,37 @@ export default function Profile() {
                     <h2 className='form-title'>Edit Profile:</h2>
                     <div className="form-group col-md-6">        
                         <label className='input-label'>Full Name</label>
-                        <input type="text" className="form-control" id='text-box' name="fullName" placeholder="John Doe" value={values.fullName} onChange={updateField} required/>
+                        <input type="text" className='text-box' id='text-box' name="fullName" placeholder="John Doe" value={values.fullName} onChange={updateField} required/>
                         {errors.fullName && <span style={{"color": "red"}}>{errors.fullName}</span>}
                     </div>
 
                     <div className="form-group col-md-6">        
                         <label className='input-label'>Username</label>
-                        <input type="text" id='text-box' name="username" placeholder={username} value={values.username} onChange={updateField} disabled />
+                        <input type="text" className='text-disabled' id='text-box' name="username" placeholder={username} value={values.username} onChange={updateField} disabled />
                     </div>
 
                     <div className="form-group col-md-6">
-                        <label>Address 1</label>
-                        <input type="text" className="form-control" name="address1" placeholder="1234 Main St" value={values.address1} onChange={updateField} required/>
+                        <label className='input-label'>Address 1</label>
+                        <input type="text" className='text-box' name="address1" placeholder="1234 Main St" value={values.address1} onChange={updateField} required/>
                         {errors.address1 && <span style={{"color": "red"}}>{errors.address1}</span>}
                     </div>
 
                     <div className="form-group col-md-6">
-                        <label>Address 2</label>
-                        <input type="text" className="form-control" name="address2" placeholder="Apartment, studio, or floor" value={values.address2} onChange={updateField} />
+                        <label className='input-label'>Address 2</label>
+                        <input type="text" className='text-box' name="address2" placeholder="Apartment, studio, or floor" value={values.address2} onChange={updateField} />
                     </div>
 
                     <div className="form-row">
 
                         <div className="form-group col-md-6">
-                            <label>City</label>
-                            <input type="text" className="form-control" name="city" value={values.city} onChange={updateField} required/>
+                            <label className='input-label'>City</label>
+                            <input type="text" className='text-box' name="city" value={values.city} onChange={updateField} required/>
                             {errors.city && <span style={{"color": "red"}}>{errors.city}</span>}
                         </div>
 
                         <div className="form-group col-md-4">
-                            <label>State</label>
-                            <select id="inputState" className="form-control" name="state" value={values.state} onChange={updateField}>
+                            <label className='input-label'>State</label>
+                            <select id="inputState" className='text-box' name="state" value={values.state} onChange={updateField}>
                                     {
                                         Object.entries(states).map(([key,value]) => {
                                             return <option key={key} value={key}>{value}</option>
@@ -162,21 +162,21 @@ export default function Profile() {
                         </div>
 
                         <div className="form-group col-md-4">    
-                            <label>Zip</label>
-                            <input type="text" className="form-control" name="zip" value={values.zip} onChange={updateField} required/>
+                            <label className='input-label'>Zip</label>
+                            <input type="text" className='text-box' name="zip" value={values.zip} onChange={updateField} required/>
                             {errors.zip && <div style={{"color": "red"}}>{errors.zip}</div>}
                         </div>
                     </div>
 
                     <div className="form-group col-md-4">
-                        <label>Phone</label>
-                        <input type="text" className="form-control" name="phone" value={values.phone} onChange={updateField} required/>
+                        <label className='input-label'>Phone</label>
+                        <input type="text" className='text-box' name="phone" value={values.phone} onChange={updateField} required/>
                         {errors.phone && <span style={{"color": "red"}}>{errors.phone}</span>}
                     </div>
 
                     <div className="form-group col-md-7">
-                        <label>Email</label>
-                        <input type="text" className="form-control" name="email" value={values.email} onChange={updateField} required/>
+                        <label className='input-label'>Email</label>
+                        <input type="text" className='text-box' name="email" value={values.email} onChange={updateField} required/>
                         {errors.email && <div style={{"color": "red"}}>{errors.email}</div>}
                     </div>
 
